851e0f119ddb57cfa281638939f8e43a990a0968
naveen v <nvenkataraman1@babson.edu>
2014-06-27 18:59:08 -0500

    cleaned up variable name

--- code/arbitragePermCombos.R
+++ code/arbitragePermCombos.R
@@ -4,10 +4,11 @@ require(gtools)
 arbitragePermCombos <- function(currCodes) {
     selectionLength <- length(currCodes)
     
-    a <- data.frame(matrix(0,nrow=1,ncol=selectionLength))
+    temp_df <- data.frame(matrix(0,nrow=1,ncol=selectionLength))
     
     for(i in 2:selectionLength) {
-        a <- rbind.all.columns(a,data.frame(permutations(n=selectionLength,r=i)))
+        temp_df <- rbind.all.columns(a,data.frame(permutations(n=selectionLength,r=i)))
     }
-       a
+       
+    temp_df
 }
