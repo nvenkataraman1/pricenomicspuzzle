68f5eae7e2381d0953775bf91a5068c78e620c77
naveen v <nvenkataraman1@babson.edu>
2014-06-27 01:00:52 -0500

    updated Readme and reformatted result output

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 pricenomicspuzzle
 =================
+
+1. Run the code in arbitrageCalc.R
+
+2. It prints a filtered dataframe with currency sequences 
+
+X1 > X2 > X3 > X4 > X5 > Value > Gain
+
+where:
+a) X1 > X2 > X3 > X4 > X4 > X5 is the arbitrage sequence
+b) Value is the arbitrage value gain
+c) Gain is the percentage gain
--- code/arbitrageCalc.R
+++ code/arbitrageCalc.R
@@ -13,12 +13,14 @@ rateseqdf[,"X5"] <- rateseqdf[,1]
 startAmt <- 100 ## starting amount in base currency
 
 for(i in 1:dim(rateseqdf)[1])
-    rateseqdf[i,"aval"] <- startAmt *
+    rateseqdf[i,"Value"] <- (startAmt *
                     conversionValue(rateseqdf[i,1],rateseqdf[i,2]) *
                         conversionValue(rateseqdf[i,2],rateseqdf[i,3]) *
                             conversionValue(rateseqdf[i,3],rateseqdf[i,4]) *
-                                conversionValue(rateseqdf[i,4],rateseqdf[i,5])
+                                conversionValue(rateseqdf[i,4],rateseqdf[i,5])) - startAmt
 
-result <- rateseqdf %>% filter(aval > startAmt)
+result <- rateseqdf %>% 
+                filter(Value > 0) %>% 
+                    mutate(Gain = paste(round((Value*100/startAmt),2),"%"))
 
 print(result)
